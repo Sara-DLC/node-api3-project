a122de709d54d39f12953abc489f489409b23053
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-04 17:28:01 -0600

    set up get requests, post request, and delete request

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,29 +1,88 @@
 const express = require('express');
+const db = require('./userDb');
+const posts = require('../posts/postDb');
 
 const router = express.Router();
 
+
+router.use(express.json());
+
 router.post('/', (req, res) => {
-  // do your magic!
+  const { text, user_id } = posts.insert(req.body)
+  .then( addText => {
+    text || user_id ? res.status(400).json({ errorMessage: "Please provide  and user id for the post."}) : res.status(201).json(addText);
+  })
+  .catch( error => {
+    console.log(error);
+    res.status(500).json({error: "There was an error while saving the post to the database"  })
+  });
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  const { text, user_id } = req.body
+  db.getById(id)
+  .then( postId => {
+    !postId.length ? posts.insert({text: text, user_id: user_id}).then( () => {
+      res.status(201).json({ message: req.body});
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'Error creating new post'})
+    }) :
+    res.status(404).json({
+      message: "The user with the specified ID does not exist."
+    });
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({errorMessage: "The post could not be saved." })
+  })
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  db.get()
+  .then(user => {
+    res.status(200).json(user);
+    })
+  .catch(error => {
+    console.log(`error on GET/api/user`, error);
+    res.status(500).json({ errorMessage: "The users information could not be retrieved."})
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id;
+  db.getById(id)
+  .then(userById => {
+    userById ? res.status(200).json(userById) : res.status(404).json({ message: "The user with the specified ID does not exist." });
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ error: "The users information could not be retrieved." });
+  });
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  db.getUserPosts(req.params.id)
+  .then(posts =>{
+    !posts ? res.status(500).json({ message: "The post with the specified ID does not exist." }) : res.status(200).json(posts);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ error: "There was an error while saving the post to the database" })
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  db.remove(req.params.id)
+    .then(removed => {
+        removed > 0 ? res.status(200).json({ message: 'post successfully deleted' }) : res.status(404).json({ message: "The post with the specified ID does not exist." });
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "The post could not be removed" })
+    })
 });
 
 router.put('/:id', (req, res) => {
@@ -33,11 +92,11 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const id = req.params.id
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  
 }
 
 function validatePost(req, res, next) {
